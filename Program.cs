d16e9d6dcec9adb3dafbaca5ab05e1cf84d4f0eb
Livia <l.amberssone.com>
2022-07-17 12:31:23 +0300

    done.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string numbers = Console.ReadLine();
+
+string[] numbersArray = numbers.Split(' ');
+
+string number1 = numbersArray[0];
+string number2 = numbersArray[1];
+int number1Int = Int32.Parse(number1);
+int number2Int = Int32.Parse(number2);
+
+if (number1Int > number2Int) {
+    Console.WriteLine(number1Int);
+}
+else {
+    Console.WriteLine(number2Int);
+}
